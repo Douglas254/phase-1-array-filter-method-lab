38ed945bc09a5168fb9897d936a647e6472d5e0a
Douglas Obara <douglasobara97@gmail.com>
2022-06-12 20:40:23 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+/*
+     returns all drivers that match the passed in name
+     returns matching drivers if case does not match but letters do
+     returns an empty array if there is no match
+*/
+const findMatching = (passedDrivers, passedNames) => {
+  return passedDrivers.filter(function (match) {
+    return match.toLowerCase() === passedNames.toLowerCase();
+  });
+};
+
+/*
+    returns a driver if beginning provided letters match
+    does not return drivers if only middle or ending letters match
+*/
+const fuzzyMatch = (passedDrivers, passedNames) => {
+  return passedDrivers.filter(function (match) {
+    return match.substring(0, 2) === passedNames.substring(0, 2);
+  });
+};
+
+// accesses the data structure to check if name matches
+const matchName = (passedDrivers, passedNames) => {
+  return passedDrivers.filter((match) => {
+    const realName = match.name;
+    return realName === passedNames;
+  });
+};
